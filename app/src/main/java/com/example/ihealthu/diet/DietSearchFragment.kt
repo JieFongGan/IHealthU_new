5f57d3a10bc96c3e2b991a1658a6209c1173edf8
KJinTarc <kuikj-wm20@student.tarc.edu.my>
2023-09-24 23:56:20 +0800

    KJ 24/9 com 4 search function

--- app/src/main/java/com/example/ihealthu/diet/DietSearchFragment.kt
+++ app/src/main/java/com/example/ihealthu/diet/DietSearchFragment.kt
@@ -1,47 +1,85 @@
 package com.example.ihealthu.diet
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.example.ihealthu.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.appcompat.widget.SearchView
+import com.example.ihealthu.databinding.FragmentDietSearchBinding
+import android.widget.TextView
 
 class DietSearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentDietSearchBinding? = null
+    private val binding get() = _binding!!
+    private val db = Firebase.firestore
+    private lateinit var searchPlanView: SearchView
+    private lateinit var dsRecyclerView: RecyclerView
+    // Initialize your adapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diet_search, container, false)
+        _binding = FragmentDietSearchBinding.inflate(inflater, container, false)
+        searchPlanView = binding.searchPlanView
+        dsRecyclerView = binding.dsRecyclerView
+        dsRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        var searchResults = mutableListOf<Map<String, Any>>()
+
+        //set up adapter
+        val adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+                return object : RecyclerView.ViewHolder(view) {}
+            }
+
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                val textView = holder.itemView as TextView
+                textView.text = searchResults[position]["dpOwnerName"] as? String ?: "N/A"
+            }
+
+            override fun getItemCount(): Int {
+                return searchResults.size
+            }
+        }
+        dsRecyclerView.adapter = adapter
+
+        // Set up search listener
+        searchPlanView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                searchFirestore(newText){ results ->
+                    searchResults = results.toMutableList()
+                    adapter.notifyDataSetChanged()
+                }
+                return true
+            }
+        })
+        return binding.root
     }
 
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DietSearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun searchFirestore(query: String?, onResults: (List<Map<String, Any>>) -> Unit) {
+        if (query == null || query.isEmpty()) {
+            onResults(emptyList())
+            return
+        }
+        db.collection("diet")
+            .whereEqualTo("dpOwnerName", query)
+            .addSnapshotListener { querySnapshot, error ->
+                if (error != null) {
+                    return@addSnapshotListener
                 }
+                val results = querySnapshot?.map { it.data } ?: emptyList()
+                onResults(results)
             }
     }
 }
