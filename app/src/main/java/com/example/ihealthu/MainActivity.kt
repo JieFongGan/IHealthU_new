b04f48354e9ff475e0d520a944b313e2931c9d98
gan jie fong <jiefonggan@gmail.com>
2023-09-30 22:50:22 +0800

    home screen need to be done

--- app/src/main/java/com/example/ihealthu/MainActivity.kt
+++ app/src/main/java/com/example/ihealthu/MainActivity.kt
@@ -1,19 +1,13 @@
 package com.example.ihealthu
 
-import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.ihealthu.databinding.ActivityMainBinding
 import com.example.ihealthu.diet.DietPlanFragment
 import com.example.ihealthu.exercise.ExerciseFragment
 import com.example.ihealthu.home.HomeFragment
-import com.example.ihealthu.MainViewModel
 import com.example.ihealthu.profile.User_Main
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
--- app/src/main/java/com/example/ihealthu/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.ihealthu
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel: ViewModel() {
-    var currentFragmentTag: String? = null
-}
